c2bed9650cf3fd2549090ba8e4887a4d7040c48f
hyeonji92 <chunghj09@gmail.com>
2019-07-16 16:40:38 +0900

    setting

--- app/src/main/java/com/bebediary/MainActivity.kt
+++ app/src/main/java/com/bebediary/MainActivity.kt
@@ -22,6 +22,14 @@ class MainActivity : AppCompatActivity() {
                 textMessage.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
+            R.id.navigation_setting -> {
+                textMessage.setText(R.string.title_setting)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_more -> {
+                textMessage.setText(R.string.title_more)
+                return@OnNavigationItemSelectedListener true
+            }
         }
         false
     }
@@ -34,4 +42,35 @@ class MainActivity : AppCompatActivity() {
         textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
+
+    /*참고 사이트
+    * https://webnautes.tistory.com/1216
+
+https://medium.com/@Patel_Prashant_/android-custom-calendar-with-events-fa48dfca8257
+
+https://dpdpwl.tistory.com/3
+
+https://www.google.com/search?q=android+calendarview+예제&rlz=1C5CHFA_enKR851KR851&oq=android+calendarview+예제&aqs=chrome..69i57.10103j0j4&sourceid=chrome&ie=UTF-8
+
+https://androi.tistory.com/136
+
+
+https://zeph1e.tistory.com/42
+
+https://drcarter.tistory.com/152
+
+https://www.dev2qa.com/android-one-time-repeat-alarm-example/
+
+https://gogorchg.tistory.com/entry/Android-AlarmManager%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-Schedule-%EA%B4%80%EB%A6%AC
+
+https://jamesdreaming.tistory.com/102
+
+https://namget.tistory.com/entry/%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EC%BB%A4%EC%8A%A4%ED%85%80-%EB%8B%AC%EB%A0%A5-%EC%98%88%EC%A0%9C-Android-Custom-CalendarView-Example
+
+https://github.com/kuluna/CalendarViewPager
+
+https://hatti.tistory.com/entry/android-calendar
+
+https://github.com/hnhariat/calendar
+    * */
 }
