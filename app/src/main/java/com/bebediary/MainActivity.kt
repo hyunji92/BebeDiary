b909dc38d54c426a4b2ae18d8176a07dd4ccf80b
jung.hyunji <jung.hyunji@nbt.com>
2019-07-19 09:52:52 +0900

    baby register

--- app/src/main/java/com/bebediary/MainActivity.kt
+++ app/src/main/java/com/bebediary/MainActivity.kt
@@ -3,13 +3,23 @@ package com.bebediary
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //등록한 아기가 업다면
+        add_baby.apply {
+            visibility = View.VISIBLE
+            setOnClickListener {
+
+            }
+        }
 
     }
 
---
+++ app/src/main/java/com/bebediary/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.bebediary
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK and Intent.FLAG_ACTIVITY_CLEAR_TASK and Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+
+            this.finish()
+        }, 2000)
+    }
+}
