6e63376070912a5cc46f0c51a1be5617abaf6ea0
jung.hyunji <jung.hyunji@nbt.com>
2019-08-20 21:51:45 +0900

    정보페이지 웹뷰 버그 수정

--- app/src/main/java/com/bebediary/info/InfoFragment.kt
+++ app/src/main/java/com/bebediary/info/InfoFragment.kt
@@ -1,5 +1,6 @@
 package com.bebediary.info
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,17 +11,16 @@ import androidx.fragment.app.Fragment
 import com.bebediary.R
 
 
+
+
 class InfoFragment : Fragment() {
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_info, container, false)
         val webView = view.findViewById(R.id.web_view) as WebView
-        // Force links and redirects to open in the WebView instead of in a browser
         webView.webViewClient = WebViewClient()
-        // Enable Javascript
-        val webSettings = webView.settings
-        webSettings.javaScriptEnabled = true
-
-        webView.loadUrl("http://www.google.com")
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.google.com/")
         return view
 
     }
